27d255df7de805a05b2772db894de6d02646638a
xiaoxion <esaurubio11@gmail.com>
2014-06-03 18:09:21 -0400

    added file structure
    added simple structure. Will work on networking tomorrow.

---
+++ home/src/main/java/com/stratazima/flickrviewer/home/MainActivity.java
@@ -0,0 +1,24 @@
+package com.stratazima.flickrviewer.home;
+
+/**
+ * Project FlickrViewer
+ * Package com.stratazima.flickrviewer.home
+ * Author  Esau
+ * Date    6/2/2014
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
